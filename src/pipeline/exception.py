04531e51f52564ffc5acec1f6c9411c854e39973
ShreyaShreedhar <1nt22ee098.shreya@nmit.ac.in>
2025-10-26 19:22:14 +0530

    EDA and Problem Statement

--- src/pipeline/exception.py
+++ src/pipeline/exception.py
@@ -1,6 +1,12 @@
 import sys
+from src.logger import logging
+
 
 def error_message_detail(error, error_detail: sys):
+    """
+    This function returns a detailed error message including
+    the script name, line number, and the actual error message.
+    """
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
@@ -10,6 +16,10 @@ def error_message_detail(error, error_detail: sys):
 
 
 class CustomException(Exception):
+    """
+    Custom Exception class to handle errors with more details.
+    """
+
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(
@@ -19,8 +29,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     try:
-        a=1/10
+        a = 1 / 0  # Intentional error for testing
     except Exception as e:
-        raise CustomException
+        logging.info("Divide by zero error occurred")
+        raise CustomException(e, sys)
